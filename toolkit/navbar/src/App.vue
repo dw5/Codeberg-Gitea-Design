201a16cd1a87b082b4ec5f908a4ad0120e6bd98a
Moritz Marquardt <git@momar.de>
2020-11-19 13:36:07 +0100

    Add a base for the navbar component (it looks very ugly currently)

---
+++ toolkit/navbar/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<Navbar msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+	name: "App",
+	components: {
+		Navbar,
+	},
+};
+</script>
